94d117bb15220a47634d8eb9684d02e16a19905e
Brandon Berney <brandon.berney@louisville.edu>
2020-10-19 18:45:28 -0400

    data: add DB context

---
+++ CardTech.ResearchSite/CardTech.ResearchSite/Data/CardTechResearchSiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CardTech.ResearchSite.Models;
+
+namespace CardTech.ResearchSite.Data
+{
+    public class CardTechResearchSiteContext : DbContext
+    {
+        public CardTechResearchSiteContext (DbContextOptions<CardTechResearchSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CardTech.ResearchSite.Models.Article> Article { get; set; }
+    }
+}
