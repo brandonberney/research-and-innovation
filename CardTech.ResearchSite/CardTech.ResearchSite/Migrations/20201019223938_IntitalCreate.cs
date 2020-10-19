077fefb83bfc1ac0123433681c25254f479aaae2
Brandon Berney <brandon.berney@louisville.edu>
2020-10-19 18:45:58 -0400

    db: add initial migrations

---
+++ CardTech.ResearchSite/CardTech.ResearchSite/Migrations/20201019223938_IntitalCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CardTech.ResearchSite.Migrations
+{
+    public partial class IntitalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Article",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Published = table.Column<DateTime>(nullable: false),
+                    LastUpdated = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    FeaturedImage = table.Column<string>(nullable: true),
+                    Featured = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Article", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Article");
+        }
+    }
+}
