01ed96165c2c226f3e1c38d5c5d545d78ed47969
Brandon Berney <brandonberney@protonmail.com>
2020-11-07 10:18:47 -0500

    startup: add microsoft authentication

--- CardTech.ResearchSite/CardTech.ResearchSite/Startup.cs
+++ CardTech.ResearchSite/CardTech.ResearchSite/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,11 +15,17 @@ namespace CardTech.ResearchSite
         }
 
         public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
+                microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
+            });
             services.Configure<RouteOptions>(options =>
             {
                 options.LowercaseUrls = true;
