ad75fd85eca6e7b74282e67b57cd5caf9919f30b
Brandon Berney <brandonberney@protonmail.com>
2020-11-08 13:29:17 -0500

    models: Add the Article model

---
+++ CardTech.ResearchSite/CardTech.ResearchSite/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardTech.ResearchSite.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Published { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastUpdated { get; set; }
+
+        public string Description { get; set; }
+        public string FeaturedImage { get; set; }
+        public bool Featured { get; set; }
+    }
+}
